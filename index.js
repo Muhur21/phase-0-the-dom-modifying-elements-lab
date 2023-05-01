07f0a71e83661393075726b5403ceb1c4ad442cc
Mehary Gebremariam <mehary.aynealem3@gmail.com>
2023-04-30 22:58:54 -0500

    lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('H1');
+newHeader.id = "victory";
+
+const YOUR_NAME = "Muhr Ge";
+newHeader.innerHTML = `${YOUR_NAME} is the champion`
